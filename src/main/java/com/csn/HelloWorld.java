38747c3b1d4bca8e105fa32c39d308af222ce0df
MakegoHckuu <101041785+MakegoHckuu@users.noreply.github.com>
2025-02-24 11:02:23 +0200

    ‘Hello, <username>!’ средствами сервера Jetty (доработал обработчик в методе handle)

--- src/main/java/com/csn/HelloWorld.java
+++ src/main/java/com/csn/HelloWorld.java
@@ -1,20 +1,39 @@
 package com.csn;
+
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class HelloWorld extends AbstractHandler {
     @Override
     public void handle(String target, Request baseRequest,
                        jakarta.servlet.http.HttpServletRequest request,
-                       jakarta.servlet.http.HttpServletResponse response) throws
-            IOException, jakarta.servlet.ServletException {
+                       jakarta.servlet.http.HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        String username = request.getParameter("username");
+
+        String responseHtml;
+        if (username != null && !username.isEmpty()) {
+            responseHtml = "<h1>Hello, " + username + "!</h1>";
+        } else {
+            responseHtml = "<h1>Enter your name</h1>" +
+                    "<form method='GET'>" +
+                    "<input type='text' name='username' required>" +
+                    "<button type='submit'>Submit</button>" +
+                    "</form>";
+        }
+
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println(responseHtml);
     }
+
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.setHandler(new HelloWorld());
